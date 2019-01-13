fde61162331fdbe2f24427aede0be31d371fe410
afw821 <afw821@gmail.com>
2019-01-13 15:03:37 -0500

    I added a readme and commented on the logic in the app.js file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Project-Manager
+
+This is a simple application that allows a user to click a button with a task on it and have it dynamically append to a task board so they can better see the tasks they have to complete and in what order.
+
+Technologies used
+
+*HTML5
+
+*CSS3
+
+*Bootstrap
+
+*jQuery
+
+How to Use
+
+Click a button and it will appear on the project board. Click the clear button to clear the project board
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
+    //Array of tasks to generate buttons
     const projectArray = [
         "Code",
         "Clean House",
@@ -10,28 +10,43 @@ $(document).ready(function(){
         "Walk the Dog",
         "Pay the Bills"
     ];
-const render = function (){
-    for(let i = 0; i < projectArray.length; i ++){
-        const projBtn = $('<button>');
-        projBtn.addClass('proj-button proj');
-        projBtn.attr('data-proj', projectArray[i]);
-        projBtn.text(projectArray[i]);
-        $('#buttons').append(projBtn);
+
+    //render function to render buttons on page when it loads
+    const render = function () {
+        //loop through array of tasks
+        for (let i = 0; i < projectArray.length; i++) {
+            //button tag set to a variable
+            const projBtn = $('<button>');
+            //add class of proj btn (to set up click event later on)
+            projBtn.addClass('proj-button');
+            //set attribute to buttons and a value to access them later
+            projBtn.attr('data-proj', projectArray[i]);
+            //set text of each button
+            projBtn.text(projectArray[i]);
+            //Append buttons when page loads
+            $('#buttons').append(projBtn);
+        }
     }
-}
-render();
+    //invoke function
+    render();
 
-const appendTask = function (){
-    const task = $('<div>').addClass('proj task-color');
-    task.text($(this).attr('data-proj'));
-    $('#display').append(task);
-}
-$('#buttons').on('click', '.proj-button', appendTask);
+    //appendTask function to append buttons to board whrn they are clicked
+    const appendTask = function () {
+        //create div inside of board for each button
+        const task = $('<div>').addClass('proj task-color');
+        //set text of each button to match button clicked by using data attr
+        task.text($(this).attr('data-proj'));
+        //append buttons to board
+        $('#display').append(task);
+    }
+    //on click event to append buttons to board using proj-button class
+    $('#buttons').on('click', '.proj-button', appendTask);
 
-const clearTask = function (){
-    $('#display').empty();
-}
-$('#clear').on('click', clearTask);
+    //clear all buttons when clear button is clicked
+    const clearTask = function () {
+        $('#display').empty();
+    }
+    $('#clear').on('click', clearTask);
 
 
 });
